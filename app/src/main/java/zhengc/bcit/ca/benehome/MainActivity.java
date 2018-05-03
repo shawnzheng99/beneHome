0ed51c881da7e626184f61ec1262fa2aedc2f145
Xin-Heng <xinheng1993@gmail.com>
2018-05-03 16:43:23 -0700

    change onBackPressed()

--- app/src/main/java/zhengc/bcit/ca/benehome/MainActivity.java
+++ app/src/main/java/zhengc/bcit/ca/benehome/MainActivity.java
@@ -152,9 +152,9 @@ public class MainActivity extends AppCompatActivity
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
             super.onBackPressed();
+        } else {
+            drawer.openDrawer(GravityCompat.START);
         }
     }
 
