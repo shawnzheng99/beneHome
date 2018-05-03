b3692a5152bbbe55633c19aea3dc1c52c256e4da
Xin-Heng <xinheng1993@gmail.com>
2018-05-03 16:27:53 -0700

    add nav bar, move map to mainactivity,add slideup for map

--- app/src/main/java/zhengc/bcit/ca/benehome/MainActivity.java
+++ app/src/main/java/zhengc/bcit/ca/benehome/MainActivity.java
@@ -2,6 +2,7 @@ package zhengc.bcit.ca.benehome;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -11,6 +12,8 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,7 +25,18 @@ import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -35,16 +49,35 @@ import java.util.HashMap;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener{
+        implements NavigationView.OnNavigationItemSelectedListener,OnMapReadyCallback {
 
     private static final String TAG = MainActivity.class.getName();
     public static ArrayList<HashMap<String, String>> formlist;
-
-
+    private GoogleMap mMap;
+    ArrayList<LatLng> markers;
+    ArrayList<HashMap<String, String>> house;
+    SupportMapFragment mapFragment;
+    private SlidingUpPanelLayout mLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLayout = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
+        mLayout.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
+            @Override
+            public void onPanelSlide(View panel, float slideOffset) {
+                Log.i(TAG, "onPanelSlide, offset " + slideOffset);
+            }
+
+            @Override
+            public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
+                Log.i(TAG, "onPanelStateChanged " + newState);
+            }
+        });
+
+        TextView t = (TextView) findViewById(R.id.name);
+        t.setText("hello");
+        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
         //------------------------nav oncreate-----------------------------------
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -65,7 +98,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
+//-------------------json load-----------------------------------------------------
         formlist = new ArrayList<>();
 
         try {
@@ -108,6 +141,11 @@ public class MainActivity extends AppCompatActivity
             e.printStackTrace();
         }
         getSupportFragmentManager().beginTransaction().replace(R.id.container, new House_list()).commitAllowingStateLoss();
+//-------------------------------map load and hide it----------------------------------------------------------
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        getSupportFragmentManager().beginTransaction().hide(mapFragment).commit();
+
     }
     //--------------------------nav method overload-----------------------------------
     @Override
@@ -149,23 +187,30 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_houselist) {
-           // vf.setDisplayedChild(0);
             getSupportFragmentManager().beginTransaction().replace(R.id.container, new House_list()).commitAllowingStateLoss();
+            hidemap();
+            mLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
         } else if (id == R.id.nav_eligibility) {
-           // vf.setDisplayedChild(1);
             getSupportFragmentManager().beginTransaction().replace(R.id.container, new Eligible()).commitAllowingStateLoss();
+            hidemap();
+            mLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
         } else if (id == R.id.nav_faq) {
-           // vf.setDisplayedChild(2);
             getSupportFragmentManager().beginTransaction().replace(R.id.container, new FAQ()).commitAllowingStateLoss();
+            hidemap();
+            mLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
         } else if (id == R.id.nav_about) {
-          //  vf.setDisplayedChild(3);
             getSupportFragmentManager().beginTransaction().replace(R.id.container, new About()).commitAllowingStateLoss();
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+            hidemap();
+            mLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
+        } else if (id == R.id.nav_map) {
+            mapFragment.getMapAsync(this);
+            /*--------------set up house list---------*/
+            setHouse();
+            /*------------------markers---------------------------*/
+            setMarkers();
+            showmap();
         }
-
+        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
@@ -194,4 +239,70 @@ public class MainActivity extends AppCompatActivity
     public static ArrayList<HashMap<String, String>> getList() {
         return formlist;
     }
+//------------------------------map method---------------------------------------------------
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        zoomToNewWest();
+
+        /*------------Marker-------------------*/
+        for (int i = 0; i < markers.size(); ++i) {
+            mMap.addMarker(new MarkerOptions()
+                    .position(markers.get(i))
+                    .title(house.get(i).get("Name"))
+
+            );
+
+        }
+        /*---------------marker listener---------*/
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Intent intent = new Intent(MainActivity.this, House_detail.class);
+                HashMap<String, String> selectHouse = new HashMap<>();
+
+                // get selected house
+                for (int j = 0; j < house.size(); ++j) {
+                    if (house.get(j).get("Name").equals(marker.getTitle())) {
+                        selectHouse = house.get(j);
+                    }
+                }
+                intent.putExtra("house", selectHouse);
+                //startActivity(intent);
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+            }
+        });
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
+            }
+        });
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+    }
+    public void zoomToNewWest() {
+        LatLng newWest = new LatLng(49.21073429331534, -122.92282036503556);
+        CameraUpdate location = CameraUpdateFactory.newLatLngZoom(newWest, 13);
+        mMap.animateCamera((location));
+    }
+    private void setHouse() {
+        house = House_list.getHouseList();
+    }
+
+    public void setMarkers() {
+        markers = new ArrayList<>();
+        for (int i = 0; i < house.size(); ++i) {
+            double y = Double.parseDouble(house.get(i).get("lon"));
+            double x = Double.parseDouble(house.get(i).get("lat"));
+            markers.add(new LatLng(x, y));
+        }
+    }
+
+    public void hidemap(){
+        getSupportFragmentManager().beginTransaction().hide(mapFragment).commit();
+    }
+    public void showmap(){
+        getSupportFragmentManager().beginTransaction().show(mapFragment).commit();
+    }
+//-------------------------------map method end---------------------------------------------------
 }
